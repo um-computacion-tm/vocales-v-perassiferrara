3832ea4f7f268ebe12cf70d3e2b7cdf26a6b82f4
v-perassiferrara <v.perassi@alumno.um.edu.ar>
2024-04-12 14:46:17 -0300

    Reestructuracion del codigo en dos archivos separados: funcion de contador de vocales y tests

--- countvocals.py
+++ countvocals.py
@@ -1,5 +1,3 @@
-import unittest
-
 def count_vocals(word):
     tuple_vocals = ("a","e","i","o","u")
     result = {}
@@ -9,44 +7,3 @@ def count_vocals(word):
                 result[letter] = 0
             result[letter] += 1
     return result
-
-class TestCountVocals(unittest.TestCase):
-
-    def test_jjjj(self):
-        result = count_vocals("jjjj")
-        self.assertEqual(result, {})
-
-    def test_aaa(self):
-        result = count_vocals("aaa")
-        self.assertEqual(result, {"a":3})
-
-    def test_palta(self):
-        result = count_vocals("palta")
-        self.assertEqual(result, {"a":2})
-
-    def test_murcielago(self):
-        result = count_vocals("murcielago")
-        self.assertEqual(result, {"a":1, "e":1, "i":1, "o":1, "u":1})
-
-    def test_electroencefalografista(self):
-        result = count_vocals("electroencefalografista")
-        self.assertEqual(result, {"a":3, "e":4, "i":1, "o":2})
-
-    def test_palta_mayus(self):
-        result = count_vocals("pAlta")
-        self.assertEqual(result, {"a":2})
-
-    def test_murcielago_mayus(self):
-        result = count_vocals("MURcielAgo")
-        self.assertEqual(result, {"a":1, "e":1, "i":1, "o":1, "u":1})
-
-    def test_electroencefalografista_mayus(self):
-        result = count_vocals("elEctrOEncefAlogrAfista")
-        self.assertEqual(result, {"a":3, "e":4, "i":1, "o":2})
-
-    def test_ornitorrinco_mayus(self):
-        result = count_vocals("OrnItorrinco")
-        self.assertEqual(result, {"i":2, "o":3})
-    
-if __name__ == "__main__":
-    unittest.main()
---
+++ test_countvocals.py
@@ -0,0 +1,43 @@
+import unittest
+from countvocals import count_vocals
+
+class TestCountVocals(unittest.TestCase):
+
+    def test_jjjj(self):
+        result = count_vocals("jjjj")
+        self.assertEqual(result, {})
+
+    def test_aaa(self):
+        result = count_vocals("aaa")
+        self.assertEqual(result, {"a":3})
+
+    def test_palta(self):
+        result = count_vocals("palta")
+        self.assertEqual(result, {"a":2})
+
+    def test_murcielago(self):
+        result = count_vocals("murcielago")
+        self.assertEqual(result, {"a":1, "e":1, "i":1, "o":1, "u":1})
+
+    def test_electroencefalografista(self):
+        result = count_vocals("electroencefalografista")
+        self.assertEqual(result, {"a":3, "e":4, "i":1, "o":2})
+
+    def test_palta_mayus(self):
+        result = count_vocals("pAlta")
+        self.assertEqual(result, {"a":2})
+
+    def test_murcielago_mayus(self):
+        result = count_vocals("MURcielAgo")
+        self.assertEqual(result, {"a":1, "e":1, "i":1, "o":1, "u":1})
+
+    def test_electroencefalografista_mayus(self):
+        result = count_vocals("elEctrOEncefAlogrAfista")
+        self.assertEqual(result, {"a":3, "e":4, "i":1, "o":2})
+
+    def test_ornitorrinco_mayus(self):
+        result = count_vocals("OrnItorrinco")
+        self.assertEqual(result, {"i":2, "o":3})
+    
+if __name__ == "__main__":
+    unittest.main()
